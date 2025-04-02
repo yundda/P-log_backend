a484791313b878a4bf22e0fdadc7c93e8748e510
kim-chulsoon <ninck90@gmail.com>
2025-04-02 11:48:22 +0900

    Docs pom.xml

---
+++ plog/src/main/java/com/example/plog/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.example.plog.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "User")
+public class PlogEntity extends BaseEntity {
+
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gno;
+
+    @Column(name = "email", unique = true, nullable=false)
+    private String email;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
