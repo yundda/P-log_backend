c6364362ec567920c77fb5126af100a13e3a9065
yundda <vvvzzang1218@naver.com>
2025-04-16 14:16:13 +0900

    [Chore] 배포 설정

--- src/main/java/com/example/plog/PlogApplication.java
+++ src/main/java/com/example/plog/PlogApplication.java
@@ -12,18 +12,33 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PlogApplication {
 	
 	public static void main(String[] args) {
+        String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+        if("local".equals(profile)){
 
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
-        System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
-        System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
-        System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
-        System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
-        System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
-        System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
-        System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
-        System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
-        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+            System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
+            System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
+            System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
+            System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
+            // System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
+            // System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
+            // System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+            System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
+            System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        }else{
+            Dotenv dotenv = Dotenv.configure().directory("/home/ubuntu/p-log/P-log_backend/deploy").ignoreIfMissing().load();
+            System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+            System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
+            // System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
+            // System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
+            // System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
+            System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
+            System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
+            System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+            System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
+            System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        }
 
 		SpringApplication.run(PlogApplication.class, args);
 	}
