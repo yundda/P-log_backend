4b51b31bfd87d424181ca52a65fd38e1098f0b8f
yundda <vvvzzang1218@naver.com>
2025-04-21 14:01:56 +0900

    [Chore]배포모드

--- src/main/java/com/example/plog/PlogApplication.java
+++ src/main/java/com/example/plog/PlogApplication.java
@@ -4,7 +4,6 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -23,14 +22,19 @@ public class PlogApplication {
             Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
             System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
             System.setProperty("MANAGER_PORT", dotenv.get("MANAGER_PORT"));
-            System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
-            System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
-            System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
-            // System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
-            // System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
-            // System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
+            // System.setProperty("DB_DEV_USERNAME", dotenv.get("DB_DEV_USERNAME"));
+            // System.setProperty("DB_DEV_PASSWORD", dotenv.get("DB_DEV_PASSWORD"));
+            // System.setProperty("DB_DEV_URL", dotenv.get("DB_DEV_URL"));
+            System.setProperty("DB_PROD_USERNAME", dotenv.get("DB_PROD_USERNAME"));
+            System.setProperty("DB_PROD_PASSWORD", dotenv.get("DB_PROD_PASSWORD"));
+            System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
             System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
             System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+            System.setProperty("AWS_S3_ACCESS_KEY", dotenv.get("AWS_S3_ACCESS_KEY"));
+            System.setProperty("AWS_S3_SECRET_ACCESS_KEY", dotenv.get("AWS_S3_SECRET_ACCESS_KEY"));
+            System.setProperty("AWS_S3_REGION", dotenv.get("AWS_S3_REGION"));
+            System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET"));
+
             TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         // }else{
         //     Dotenv dotenv = Dotenv.configure().directory("/home/ubuntu/p-log/P-log_backend/deploy").ignoreIfMissing().load();
@@ -44,6 +48,10 @@ public class PlogApplication {
         //     System.setProperty("DB_PROD_URL", dotenv.get("DB_PROD_URL"));
         //     System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
         //     System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        // System.setProperty("AWS_S3_ACCESS_KEY", dotenv.get("AWS_S3_ACCESS_KEY"));
+        // System.setProperty("AWS_S3_SECRET_ACCESS_KEY", dotenv.get("AWS_S3_SECRET_ACCESS_KEY"));
+        // System.setProperty("AWS_S3_REGION", dotenv.get("AWS_S3_REGION"));
+        // System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET"));
         //     TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         // }
 
